f9e9ae0c97895f5063c34053a1823c02430e660b
Christoph Mai <support@agentur2c.de>
2026-08-28 12:11:51 +0200

    Version 1.1 mit Cache-File

--- quick-install.sh
+++ quick-install.sh
@@ -5,10 +5,14 @@ CHKSCRIPT="froxlor_version"
 
 INSTPATH="/usr/lib/check_mk_agent/local"
 
-rm $INSTPATH/*/$CHKSCRIPT
+# Alte Installationen aus verschachtelten Intervall-Ordnern (z. B. /10800/) löschen
+rm -f $INSTPATH/*/$CHKSCRIPT
 
+# Verzeichnis anlegen (falls nicht existent) und Skript im Hauptordner platzieren
 mkdir -p $INSTPATH
 wget -O $INSTPATH/$CHKSCRIPT $CHKSCRIPTURL
 chmod +x $INSTPATH/$CHKSCRIPT
 
-echo "Please use Setup -> Service Discovery on your Checkmk for this host"
+echo "Installation abgeschlossen!"
+echo "Bitte nutze Setup -> Service Discovery in Checkmk für diesen Host, um die Änderungen zu übernehmen."
+
